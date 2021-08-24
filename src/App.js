5864355ee76761de7d43e9baa34b62065e18385c
monicaiyb <monicaiyb@gmail.com>
2021-08-24 23:47:59 +0300

    Add new branch learn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
-import Message from './components/Message1';
-import Button from './components/Button';
-import Number from './components/NumberArray'
+
+import Contact from './components/contact';
+
 
 function App() {
   return (
     <div id="root">
-      <Header />
-      <Button />
-      <Message />
-      <Number/>
+      <Contact/>
 
 
     </div>
---
+++ src/components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+            hello World
+        </div>
+    )
+}
+
+export default contact
